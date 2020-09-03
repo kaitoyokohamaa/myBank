ddd2325071b4638a41ec37da984f56d78f45fbc7
【kaito yokohama】 <s1A121700885@toyo.jp>
2020-09-03 15:27:07 +0900

    sucees run storybook

--- src/components/atoms/Input/index.tsx
+++ src/components/atoms/Input/index.tsx
@@ -1,39 +1,8 @@
-import React from "react";
-const Input = (props: any) => {
-    let inputElement = null;
-
-    switch (props.elementType) {
-        case "email":
-            inputElement = (
-                <React.Fragment>
-                    <label className="form-label">
-                        {props.elementConfig.label}
-                    </label>
-                    <input
-                        {...props.elementConfig}
-                        value={props.value}
-                        onChange={props.changed}
-                        onKeyDown={props.onKeyPressed}
-                    />
-                </React.Fragment>
-            );
-            break;
-        case "password":
-            inputElement = (
-                <React.Fragment>
-                    <label className="form-label">
-                        {props.elementConfig.label}
-                    </label>
-                    <input
-                        {...props.elementConfig}
-                        value={props.value}
-                        onChange={props.changed}
-                        onKeyDown={props.onKeyPressed}
-                    />
-                </React.Fragment>
-            );
-            break;
-    }
-    return <div className="form-group">{inputElement}</div>;
-};
-export default Input;
+import React from 'react'
+import "./input.css"
+export default function Input(props: any) {
+    const { size = "medium", ...rest } = props
+    return (
+        <input className={`Input ${size}`} {...rest} />
+    )
+}
