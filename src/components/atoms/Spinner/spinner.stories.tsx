22a7f10f58ef3b83191787d17de970a055a2da17
【kaito yokohama】 <s1A121700885@toyo.jp>
2020-09-04 18:04:35 +0900

    faile spinner add storybook

---
+++ src/components/atoms/Spinner/spinner.stories.tsx
@@ -0,0 +1,12 @@
+
+import React from 'react'
+import spinner from './spinner'
+
+export default {
+    title: 'Atom/spinner',
+    component: spinner
+}
+
+export const Spinner = <spinner />
+
+
---
+++ src/components/atoms/Spinner/spinner.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import styles from "./spinner.module.css"
+export default function spinner() {
+    return (
+        <div className={styles.spinner}>hoge</div>
+    )
+}
