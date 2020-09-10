5abc376801b244fa0140c6a577ffc5720ae5daae
【kaito yokohama】 <s1A121700885@toyo.jp>
2020-09-10 16:01:09 +0900

    sucess to use cssModule

--- .storybook/main.tsx
+++ .storybook/main.tsx
@@ -4,9 +4,11 @@ module.exports = {
     "@storybook/preset-create-react-app",
     "@storybook/addon-actions",
     "@storybook/addon-links",
+    'storybook-css-modules-preset',
     "@storybook/addon-docs",
     "@storybook/addon-knobs",
     "@storybook/addon-viewport",
+    '@storybook/preset-typescript',
     "@storybook/addon-a11y"
   ]
 };
