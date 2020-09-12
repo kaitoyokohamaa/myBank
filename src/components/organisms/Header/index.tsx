edea45f028a8f93dcfce1f0de19d722b72890306
【kaito yokohama】 <s1A121700885@toyo.jp>
2020-09-12 17:34:02 +0900

    add logout

---
+++ src/components/organisms/Header/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import Logo from "../../atoms/Logo"
+import styles from "./header.module.css"
+import { Link } from "react-router-dom"
+import * as firebase from "firebase/app";
+export default function index() {
+    const logOut = () => {
+        alert("ログアウトします")
+        firebase.auth().signOut();
+    }
+    return (
+        <div>
+            <header className={styles.header}>
+                <Logo />
+                <div className={styles.linkWrap}>
+                    <li>
+                        <Link className={styles.link} to="/">グラフ</Link>
+                    </li>
+                    <li>
+                        <Link className={styles.link} to="/">カレンダー</Link>
+                    </li>
+                    <li>
+                        <Link className={styles.link} to="/" onClick={logOut} > ログアウト</Link>
+                    </li>
+                </div>
+            </header>
+
+        </div>
+    )
+}
