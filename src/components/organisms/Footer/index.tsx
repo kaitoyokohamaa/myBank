1d16dcbdd1874968906f37f3c8c0d4cf8c72af67
【kaito yokohama】 <s1A121700885@toyo.jp>
2020-09-13 17:37:16 +0900

    add contact form

--- src/components/organisms/Footer/index.tsx
+++ src/components/organisms/Footer/index.tsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import Coment from "../../atoms/Coment"
-import styles from "./footer.module.css"
+import styles from "./footer.module.css";
+import { Link } from "react-router-dom"
 export default function index() {
     return (
-        <div className={styles.imgArea}>
-            <Coment>
-                誰か僕を雇ってください
-            </Coment>
-        </div>
+        <footer className={styles.foot}>
+            <div className={styles.imgArea}>
+                <a
+                    className={styles.link}
+                    href=" https://docs.google.com/forms/u/0/d/e/1FAIpQLSfdMK4UtMRH6L8Fiz7m1xbU4pfaRBxOc3LAn5qkrj3jDYKO4A/formResponse?pli=1"
+                >
+                    <Coment>
+                        誰か僕を雇ってください
+                </Coment>
+                </a>
+            </div>
+        </footer >
     )
 }
