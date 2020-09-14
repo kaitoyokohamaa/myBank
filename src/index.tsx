eeaa071dc50faa6a3e1a09575628ce544a9ca589
【kaito yokohama】 <s1A121700885@toyo.jp>
2020-09-14 16:39:07 +0900

    add normalize.css

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import "normalize.css";
 ReactDOM.render(
     <BrowserRouter>
         <App />
