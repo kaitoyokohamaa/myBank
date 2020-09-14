a6611f3cf916a2d366e8319f03271b3b77513acc
【kaito yokohama】 <s1A121700885@toyo.jp>
2020-09-14 16:20:20 +0900

    delete calendar link

--- src/components/organisms/Header/index.tsx
+++ src/components/organisms/Header/index.tsx
@@ -20,9 +20,6 @@ export default function index() {
                         <Link className={styles.link} to="/graph">グラフ</Link>
                     </li>
                     <li>
-                        <Link className={styles.link} to="/calendar">カレンダー</Link>
-                    </li>
-                    <li>
                         <Link className={styles.link} to="/" onClick={logOut} > ログアウト</Link>
                     </li>
                 </div>
