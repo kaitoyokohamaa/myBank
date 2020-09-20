f8911b0d95986ef60109ed0acad34b21c2fc3a76
【kaito yokohama】 <s1A121700885@toyo.jp>
2020-09-20 17:38:19 +0900

    fix chakra-ui Button

--- src/components/atoms/Chakra-ui/button.stories.tsx
+++ src/components/atoms/Chakra-ui/button.stories.tsx
@@ -6,14 +6,13 @@ export default {
     title: 'Chakra/Button',
     component: Button
 }
-"storybook": "start-storybook -p 9001"
+
 export const signBtn = () => (
     <Button variantColor='green' >
         登録
     </Button>
 )
-"@storybook/cli": "^6.0.21",
-    "@storybook/react": "^6.0.21",
+
 export const homeBtn = () => (
     <Button  >
         登録
