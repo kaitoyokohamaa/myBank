3d8150553690c8fa3abdaa4e6f66c1f2447a3389
【kaito yokohama】 <s1A121700885@toyo.jp>
2020-09-21 15:30:56 +0900

    fix firebase err

--- src/firebase.tsx
+++ src/firebase.tsx
@@ -3,12 +3,12 @@ import "firebase/firestore";
 import "firebase/auth";
 
 const app = {
-    apiKey: process.env.REACT_APP_FIREBASE_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
-    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID
+    apiKey:process.env.REACT_APP_FIREBASE_KEY,
+    authDomain:process.env.REACT_APP_FIREBASE_DOMAIN,
+    databaseURL:process.env.REACT_APP_FIREBASE_DATABASE,
+    projectId:process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket:process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId:process.env.REACT_APP_FIREBASE_SENDER_ID
 };
 firebase.initializeApp(app);
 export const db = firebase.firestore();
