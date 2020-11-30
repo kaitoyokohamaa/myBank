e348cdf05d9b079f433ef2631cbfe9e354ddf15e
yoko <s1A121700885@toyo.jp>
2020-11-30 16:43:57 +0900

    refactore:firebaseの処理を別フォルダーで管理

---
+++ src/functions/useHome.tsx
@@ -0,0 +1,62 @@
+import React, { useState, useEffect } from "react";
+import firebase from "../firebase";
+export function useHome() {
+  const [budget, setBudget] = useState<firebase.firestore.DocumentData>();
+  const [income, setIncome] = useState<number>();
+  const [expence, setExpence] = useState<number>();
+  const [totalBudget, setTotalBudget] = useState<number>();
+  useEffect(() => {
+    return firebase
+      .auth()
+      .onAuthStateChanged(async (usr: firebase.User | null) => {
+        if (!usr) {
+          alert("新規登録をしてください");
+        } else {
+          firebase
+            .firestore()
+            .collection("budget")
+            .orderBy("createdAt", "desc")
+            .onSnapshot((querySnapshot: firebase.firestore.QuerySnapshot) => {
+              let storeBudget: firebase.firestore.DocumentData[] = [];
+              let storeIncome: number[] = [];
+              let storeExpence: number[] = [];
+              querySnapshot.forEach(
+                async (docs: firebase.firestore.DocumentData) => {
+                  const showBudget = docs.data();
+                  storeBudget.push(showBudget);
+                  //収入の中身を足す
+                  if (showBudget.type === "inc") {
+                    const incomeMoney: number = await showBudget.money;
+                    storeIncome.push(incomeMoney);
+                    const reducer = (sum: number, currentValue: number) =>
+                      sum + currentValue;
+                    const sumMoney = storeIncome.reduce(reducer);
+                    //足された収入の中身をstateで管理
+                    setIncome(sumMoney);
+                  }
+                  //支出の中身を足す
+                  else if (showBudget.type === "exp") {
+                    const expenceMoney: number = showBudget.money;
+                    storeExpence.push(expenceMoney);
+                    const reducer = (sum: number, currentValue: number) =>
+                      sum + currentValue;
+                    const decMoney = storeExpence.reduce(reducer);
+                    //足された支出の中身をstateで管理
+                    setExpence(decMoney);
+                  }
+                }
+              );
+              setBudget(storeBudget);
+            });
+        }
+      });
+  }, [setExpence, setIncome]);
+  // 収入と支出を足す
+  useEffect(() => {
+    if (income && expence) {
+      setTotalBudget(income - expence);
+    }
+  }, [income, expence]);
+
+  return [{ totalBudget, income, expence, budget }] as const;
+}
