78db9bf94f07f2c4e8fcc4126ac0971e7c2bebef
yoko <s1A121700885@toyo.jp>
2020-12-27 08:20:20 +0900

    feat:firebaseへのデプロイ設定

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9d01099de2160837c6ca4717c3c27d19.js"
+  "/precache-manifest.0c8831e88f878c84abded970f791306f.js"
 );
 
 self.addEventListener('message', (event) => {
