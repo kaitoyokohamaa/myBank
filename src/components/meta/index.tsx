7b163771c4e0c521941c9c745beb50862d82b0e3
yoko <s1A121700885@toyo.jp>
2021-04-03 12:12:25 +0900

    fix:fix ogp size

--- src/components/meta/index.tsx
+++ src/components/meta/index.tsx
@@ -9,7 +9,13 @@ export const Meta: VFC<Props> = ({ title, image }) => {
   return (
     <head>
       <title>{title}</title>
-      <meta property="og:image" content={image ? image : "/img/pig.jpeg"} />
+      <meta name="twitter:image" content={image ? image : "/img/pig.jpeg"} />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta
+        property="og:image"
+        className="w-full h-full"
+        content={image ? image : "/img/pig.jpeg"}
+      />
     </head>
   );
 };
