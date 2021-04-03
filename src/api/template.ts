a6c366a1d07c38cc6508fdcc7719aeb9ae451c7f
yoko <s1A121700885@toyo.jp>
2021-04-03 17:27:23 +0900

    fix: js apply

--- src/api/template.ts
+++ src/api/template.ts
@@ -19,58 +19,48 @@ const mono = readFileSync(`${__dirname}/../_fonts/Vera-Mono.woff2`).toString(
 function getCss(theme: string, fontSize: string) {
   let background = "white";
   let foreground = "black";
-  let radial = "lightgray";
 
   if (theme === "dark") {
     background = "black";
     foreground = "white";
-    radial = "dimgray";
   }
   return `
-    @import url('https://fonts.googleapis.com/css?family=M+PLUS+1p');
+    @import url('https://fonts.googleapis.com/css?family=Noto+Sans+JP');
     @font-face {
         font-family: 'Inter';
         font-style:  normal;
         font-weight: normal;
         src: url(data:font/woff2;charset=utf-8;base64,${rglr}) format('woff2');
     }
-
     @font-face {
         font-family: 'Inter';
         font-style:  normal;
         font-weight: bold;
         src: url(data:font/woff2;charset=utf-8;base64,${bold}) format('woff2');
     }
-
     @font-face {
         font-family: 'Vera';
         font-style: normal;
         font-weight: normal;
         src: url(data:font/woff2;charset=utf-8;base64,${mono})  format("woff2");
       }
-
     body {
         background: ${background};
-        background-image: radial-gradient(circle at 25px 25px, ${radial} 2%, transparent 0%), radial-gradient(circle at 75px 75px, ${radial} 2%, transparent 0%);
-        background-size: 100px 100px;
         height: 100vh;
         display: flex;
         text-align: center;
         align-items: center;
         justify-content: center;
     }
-
     code {
         color: #D400FF;
-        font-family: 'Vera';
+        font-family: 'Vera', 'M PLUS 1p';
         white-space: pre-wrap;
         letter-spacing: -5px;
     }
-
     code:before, code:after {
         content: '\`';
     }
-
     .logo-wrapper {
         display: flex;
         align-items: center;
@@ -78,21 +68,17 @@ function getCss(theme: string, fontSize: string) {
         justify-content: center;
         justify-items: center;
     }
-
     .logo {
         margin: 0 75px;
     }
-
     .plus {
         color: #BBB;
         font-family: Times New Roman, Verdana;
         font-size: 100px;
     }
-
     .spacer {
-        margin: 150px;
+        margin: 75px;
     }
-
     .emoji {
         height: 1em;
         width: 1em;
@@ -101,16 +87,17 @@ function getCss(theme: string, fontSize: string) {
     }
     
     .heading {
-        font-family: 'M PLUS 1p', 'Inter', sans-serif;
+        font-family: 'Noto Sans JP', 'Inter', sans-serif;
         font-size: ${sanitizeHtml(fontSize)};
         font-style: normal;
+        font-weight: 400;
         color: ${foreground};
-        line-height: 1.8;
+        line-height: 1.5;
     }`;
 }
 
 export function getHtml(parsedReq: ParsedRequest) {
-  const { text, theme, md, fontSize, images, widths, heights } = parsedReq;
+  const { text, theme, md, fontSize } = parsedReq;
   return `<!DOCTYPE html>
 <html>
     <meta charset="utf-8">
@@ -122,15 +109,6 @@ export function getHtml(parsedReq: ParsedRequest) {
     <body>
         <div>
             <div class="spacer">
-            <div class="logo-wrapper">
-                ${images
-                  .map(
-                    (img, i) =>
-                      getPlusSign(i) + getImage(img, widths[i], heights[i])
-                  )
-                  .join("")}
-            </div>
-            <div class="spacer">
             <div class="heading">${emojify(
               md ? marked(text) : sanitizeHtml(text)
             )}
@@ -138,18 +116,4 @@ export function getHtml(parsedReq: ParsedRequest) {
         </div>
     </body>
 </html>`;
-}
-
-function getImage(src: string, width = "auto", height = "225") {
-  return `<img
-        class="logo"
-        alt="Generated Image"
-        src="${sanitizeHtml(src)}"
-        width="${sanitizeHtml(width)}"
-        height="${sanitizeHtml(height)}"
-    />`;
-}
-
-function getPlusSign(i: number) {
-  return i === 0 ? "" : '<div class="plus">+</div>';
 }
